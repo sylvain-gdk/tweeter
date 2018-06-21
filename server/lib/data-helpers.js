1dcd351de2171e8f8749677be7ff09912711dba7
Sylvain Goedike <sgoedike@videotron.ca>
2018-06-21 20:10:48 +0000

    Updated saveTweet function to send to mongodb

--- server/lib/data-helpers.js
+++ server/lib/data-helpers.js
@@ -9,11 +9,16 @@ module.exports = function makeDataHelpers(db) {
 
     // Saves a tweet to `db`
     saveTweet: function(newTweet, callback) {
-      simulateDelay(() => {
-        db.tweets.push(newTweet);
+        db.collection('tweets').insertOne(newTweet);
         callback(null, true);
-      });
     },
+
+    // saveTweet: function(newTweet, callback) {
+    //   simulateDelay(() => {
+    //     db.tweets.push(newTweet);
+    //     callback(null, true);
+    //   });
+    // },
 
     // Get all tweets in `db`, sorted by newest first
     getTweets: function(callback) {
