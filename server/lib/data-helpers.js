70a0a6c7c3c1d1f552909f5da7a0bf4ea15f01b6
Sylvain Goedike <sgoedike@videotron.ca>
2018-06-21 20:03:43 +0000

    Connected to mongodb instead if reading from JSON file and updated getTweets function

--- server/lib/data-helpers.js
+++ server/lib/data-helpers.js
@@ -17,11 +17,15 @@ module.exports = function makeDataHelpers(db) {
 
     // Get all tweets in `db`, sorted by newest first
     getTweets: function(callback) {
-      simulateDelay(() => {
-        const sortNewestFirst = (a, b) => a.created_at - b.created_at;
-        callback(null, db.tweets.sort(sortNewestFirst));
-      });
+      db.collection('tweets').find().toArray(callback);
     }
+
+    // getTweets: function(callback) {
+    //   simulateDelay(() => {
+    //     const sortNewestFirst = (a, b) => a.created_at - b.created_at;
+    //     callback(null, db.tweets.sort(sortNewestFirst));
+    //   });
+    // }
 
   };
 }
